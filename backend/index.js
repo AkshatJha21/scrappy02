c017e0361e9f19e7ff216e905506d59d04a46e3a
AkshatJha21 <akshatj2134@gmail.com>
2024-04-16 14:31:11 +0530

    route def

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get('/all', async (req, res) => {
+
+});
+
+app.post('/new', async (req, res) => {
+
+});
+
+app.delete('/remove', async (req, res) => {
+
+});
+
 app.listen(port, (req, res) => {
     console.log(`Listening on port ${port}`);
 });
