445b33e30a221ac90ef7d5c0f5cd5f8d3bf2d83a
AkshatJha21 <akshatj2134@gmail.com>
2024-04-18 05:55:09 +0530

    zod added

--- backend/types.js
+++ backend/types.js
@@ -1,2 +1,11 @@
 const zod = require("zod");
 
+const addExpense = zod.object({
+    title: zod.string().min(1),
+    category: zod.string(),
+    amount: zod.number().int().gt(0, { message: 'Amount must be greater than 0'})
+})
+
+module.exports = {
+    addExpense
+}
