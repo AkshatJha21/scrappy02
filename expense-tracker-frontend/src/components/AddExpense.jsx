50d7f79f330a26e25140902e26c720c48483682d
AkshatJha21 <akshatj2134@gmail.com>
2024-04-27 10:18:21 +0530

    411 length required

--- expense-tracker-frontend/src/components/AddExpense.jsx
+++ expense-tracker-frontend/src/components/AddExpense.jsx
@@ -4,17 +4,48 @@ import './addExpense.css'
 const AddExpense = () => {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
   return (
-    // INPUT FIELDS TO BE ADDED
-    // CAUTION: SELECT CATEGORY NOT WORKING LAST TIME EVEN THOUGH ROUTES ARE WORKING
     <div className='container'>
       <input type="text" placeholder='Title' onChange={(e) => {
         setTitle(e.target.value);
       }}/>
+      <div className='categorySelector'>
+        <label htmlFor="category">Category</label>
+        <select name="category" id="category" onChange={(e) => {
+          setCategory(e.target.value);
+        }}>
+          <option value="travel">Travel</option>
+          <option value="food">Food</option>
+          <option value="clothing">Clothing</option>
+          <option value="education">Education</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
       <input type="number" placeholder='Amount' onChange={(e) => {
         setAmount(e.target.value);
       }}/>
-      <button>Add Expense</button>
+      <button onClick={() => {
+        console.log(category);
+        console.log(title);
+        console.log(amount);
+        fetch("http://localhost:3000/new", {
+          method: "POST",
+          body: JSON.stringify({
+            title: title,
+            category: category,
+            amount: amount
+          }),
+          headers: {
+            "Content-type": "application/json",
+            "Content-length": 0
+            // CHECK CONTENT-LENGTH AND SELECT TAG ERRORS ON INTERNET
+          }
+        }).then(async function (res) {
+          await res.json();
+          console.log("Expense added successfully");
+        })
+      }}>Add Expense</button>
     </div>
   )
 }
