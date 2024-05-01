70df0b2cba6f9bf8b6bbb811fbde95838bd9b959
AkshatJha21 <akshatj2134@gmail.com>
2024-05-01 13:18:14 +0530

    delete init

--- expense-tracker-frontend/src/components/AllExpenses.jsx
+++ expense-tracker-frontend/src/components/AllExpenses.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from "axios"
 
 const AllExpenses = ({ expenses, filterCategory }) => {
     const filteredExpenses = filterCategory ? expenses.filter(expense => expense.category === filterCategory) : expenses;
@@ -10,6 +11,12 @@ const AllExpenses = ({ expenses, filterCategory }) => {
                     <h3>{expense.title}</h3>
                     <p>{expense.category}</p>
                     <p>{expense.amount}</p>
+                    {/* SAMPLE FUNCTION */}
+                    <button onClick={() => {
+                        axios.delete("http://localhost:3000/remove", {
+                            id: expense._id
+                        });
+                    }}>Delete</button>
                 </div>
             )
         })}
