28fa7e20bece122b980b40b2ca7405e96e30676d
AkshatJha21 <akshatj2134@gmail.com>
2024-05-31 09:47:26 +0530

    edit function active

--- expense-tracker-frontend/src/components/EditExpense.jsx
+++ expense-tracker-frontend/src/components/EditExpense.jsx
@@ -1,21 +1,48 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 const EditExpense = ({ expense, closeDialog }) => {
     const [title, setTitle] = useState(expense.title);
-    const [description, setDescription] = useState(expense.description);
+    const [amount, setAmount] = useState(expense.amount);
     const [category, setCategory] = useState(expense.category);
-    
+
+    const handleUpdate = () => {
+        const amt = parseFloat(amount);
+        axios.put("http://localhost:3000/edit", {
+            id: expense._id,
+            title,
+            category,
+            amount: amt
+        }).then((res) => {
+            console.log("Expense updated successfully");
+            closeDialog();
+            window.location.reload();
+        }).catch((err) => {
+            console.log("Error: ", err);
+        });
+    }
+
   return (
     <div className='dialog'>
         <div className='dialog-content'>
             <h2>Update Expense</h2>
             <div className='dialog-inputs'>
-                <input type="text" />
-                <select></select>
-                <input type="text" />
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <select
+                    className='categorySelector'
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                    <option value="">Select category</option>
+                    <option value="Food">Food</option>
+                    <option value="Transportation">Transportation</option>
+                    <option value="Utilities">Utilities</option>
+                    <option value="Entertainment">Entertainment</option>
+                </select>
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}/>
             </div>
             <div className='dialog-actions'>
-                <button>Update</button>
+                <button onClick={handleUpdate}>Update</button>
                 <button onClick={closeDialog}>Close</button>
             </div>
         </div>
